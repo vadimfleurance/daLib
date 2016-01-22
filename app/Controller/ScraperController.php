b02c53f21538e1f29d539de133b946874f863bed
vadim <supervad@gmail.com>
2016-01-22 16:14:31 +0100

    created scraperController and route to controller waiting to implant code

---
+++ app/Controller/ScraperController.php
@@ -0,0 +1,16 @@
+<?php
+namespace Controller;
+
+use \W\Controller\Controller;
+use Sunra\PhpSimple\HtmlDomParser;
+
+class ScraperController extends Controller
+{
+	public function globalScraper()
+	{
+		/*$dom = HtmlDomParser::file_get_html( $file_name );*/
+		echo 'bien arrivé';
+	}
+
+
+}
