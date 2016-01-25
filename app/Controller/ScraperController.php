578aa217b020ff417cf8408c7a254e943a5da3c8
Flop2691 <flo.bardouillet@gmail.com>
2016-01-25 10:58:07 +0100

    create method pageScraper

--- app/Controller/ScraperController.php
+++ app/Controller/ScraperController.php
@@ -35,6 +35,7 @@ class ScraperController extends Controller
 			preg_match( $regIdMovie , $urlMovie , $matches );
 
 			$link = "http://www.imdb.com/title/' . $matches[0] . '/";
+			$this->pageScraper($urlImdb);
 		}
 
 		
@@ -42,10 +43,116 @@ class ScraperController extends Controller
 		//pour en extraire l'ensemble des donnees du film
 	}
 
-	public function pageScraper()
+	public function pageScraper($urlImdb)
 	{
 		//extraire toutes les donnees necessaires d'une page d'un film
+		//initialisation du tableau qui contiendra toutes les données du film
+		$movie = ["title" => "", "synopsis" => "", "duration" => "", "year" => "", "imbdRef" => "", "rating" => "", "cover" => "", "directors" => "", "writers" =>"", "stars" => "", "genres" => ""];
+		//utilisation de la fonction trim pour supprimer les espaces en début et en fin de chaine
+		$urlImdb = trim($urlImdb);
+
+		// si l'url ou l'id passé a une syntaxe valide
+		if (preg_match("!^http://www.imdb.com/title/[t]{2}\d{7}/$!", $urlImdb) || preg_match("!^http://www.imdb.com/title/[t]{2}\d{7}/\?.*!", $urlImdb) || preg_match("!^[t]{2}\d{7}$!", $urlImdb)){
+
+			//vérifie si le texte passé est un id d'IMDb  (sans les slash autour) de type "ttxxxxxxx" ou les x sont des chiffres de 0 à 9 et le traite si c'est le cas
+			if (preg_match("!^[t]{2}\d{7}$!", $urlImdb)){
+				$urlImdb = "http://www.imdb.com/title/" . $urlImdb . "/";
+			}
+
+			//vérifie si le texte passé est une URL d'un film sur IMDb
+			if (preg_match("!^http://www.imdb.com/title/[t]{2}\d{7}/$!", $urlImdb)){
+				// $urlImdb = $urlImdb;
+			}
+
+			//vérifie si le texte passé est une URL avec des paramètres et supprime les paramètres si c'est le cas
+			if(preg_match("!^http://www.imdb.com/title/[t]{2}\d{7}/\?.*!", $urlImdb)){
+				$urlImdb = preg_replace("/\?.*/", "", $urlImdb);
+			}
+
+			//permet d'avoir le titre en anglais
+			//création d'une entête HTTP
+			$opts = ['http' => [
+			    'method'=>"GET",
+			    'header'=>"Accept-language: en\r\n"
+			]];
+
+			$context = stream_context_create($opts);
+
+			// récupère le contenu html de l'url donnée en ajoutant à l'url l'entête HTTP créée plus haut
+			$content = file_get_contents($url, false, $context);
+
+			//crée un objet de la classe simple_html_dom et lui passe le contenu html de la page imdb
+			$html = str_get_html($content);
+
+			//récupération des différents champs
+			//utilisation de la fonction trim pour supprimer les espaces en début et en fin de chaine
+
+			//titre du film
+			$title = trim($html->find('span[itemprop=name]',0)->plaintext);
+
+			//synopsis
+			$synopsisTmp = $html->find("p[itemprop=description]",0);
+			if (!empty($synopsisTmp)){
+				$movie["synopsis"] = trim($synopsisTmp->plaintext);
+			}
+
+			//durée
+			$durationTmp = $html->find("time[itemprop=duration]",0);
+			if (!empty($durationTmp)){
+				$movie["duration"] = trim(str_replace(" min", "", $durationTmp->plaintext));
+			}
+			// $duration = trim(str_replace(" min", "", $html->find("time[itemprop=duration]",0)->plaintext));
+
+			//année
+			$yearTmp = $html->find(".header", 0)->find('.nobr', 0);
+			if (!empty($yearTmp)){
+				$movie["year"] = trim($yearTmp->find("a", 0)->plaintext);
+			}
+
+			//référence IMDb
+			$imdbRef = trim($html->find("meta[property=pageId]",0)->getAttribute('content'));
+
+			//évaluation
+			$ratingTmp = $html->find("div[class=titlePageSprite star-box-giga-star]",0);
+			if (!empty($ratingTmp)){
+				$movie["rating"] = trim($ratingTmp->plaintext);
+			}
+
+			//url de la cover sans le suffixe ni l'extension
+			$coverTmp = $html->find("#title-overview-widget",0)->find("img",0);
+			if (!empty($coverTmp)){
+				$movie["cover"] = trim(preg_replace("/@.*/", "", $coverTmp->src));
+			}
+
+			//réalisateurs
+			$directorsTmp = $html->find("div[itemprop=director]",0);
+			if(!empty($directorsTmp)){
+				$movie["directors"] = $directorsTmp->find("span[itemprop=name]");
+			}
+
+			//scénaristes
+			$writersTmp = $html->find("div[itemprop=creator]",0);
+			if(!empty($writersTmp)){
+				$movie["writers"] = $writersTmp->find("span[itemprop=name]");
+			}
+
+			//acteurs
+			$starsTmp = $html->find("div[itemprop=actors]",0);
+			if (!empty($starsTmp)){
+				$movie["stars"] = $starsTmp->find("span[itemprop=name]");
+			}
+
+			//genres
+			$genresTmp = $html->find("span[itemprop=genre]");
+			if (!empty($genresTmp)){
+				$movie["genres"] = $genresTmp;		
+			}
+		}
+		
+		//sinon (erreur)
+		else{
+			//affichage de l'erreur d'import (pas encore créé)
+			// $this->show("movie/addMovie", [$error => "L'URL n'est pas correcte");
+		}
 	}
-
-
 }
