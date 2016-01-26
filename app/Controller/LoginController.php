6d07bee1160b6189a2b4fcbe7c461317561aa81c
SweetJeez <etienne.dutour@gmail.com>
2016-01-26 17:37:12 +0100

    LoginController created with nothing inside

---
+++ app/Controller/LoginController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Controller;
+
+use \W\Controller\Controller;
+
+class LoginController extends Controller
+{
+
+	public function logIn()
+	{
+
+	}
+
+	public function logOut()
+	{
+
+	}
+
+	public function rememeberMe()
+	{
+		
+	}
+
+
+}
--- app/routes.php
+++ app/routes.php
@@ -2,7 +2,12 @@
 	
 	$w_routes = array(
 		['GET', '/', 'Default#home', 'home'],
+
+		//Registration
 		['GET|POST', '/registration/', 'Registration#register', 'register' ],
 		['GET|POST', '/registration-confirmed/', 'Registration#success', 'registration_success' ],
+
+		//Login
+		['GET|POST', '/login/', 'Login#logIn', 'login' ],		
 
 	);
