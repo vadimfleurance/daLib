93ede8b1715d1974abfcf7c92d65d4d4dafd3553
vadim <supervad@gmail.com>
2016-01-27 14:41:57 +0100

    method getInfos done return clean  array to use later

--- app/Manager/MovieManager.php
+++ app/Manager/MovieManager.php
@@ -20,7 +20,7 @@ class MovieManager extends \W\Manager\Manager
 
 	public function getInfos($id)
 	{
-		$moviesInfos =[];
+		
 
 		$sql = 'SELECT 
 				movies.title,
@@ -29,7 +29,7 @@ class MovieManager extends \W\Manager\Manager
 				movies.year,
 				movies.imdbRating,
 				movies.cover,
-				genres.name,
+				genres.name AS genre,
 				movies__humans.role,
 				humans.name
 				FROM movies
@@ -47,10 +47,54 @@ class MovieManager extends \W\Manager\Manager
 		$stmt->bindValue(':id', $id);
 		$stmt->execute();
 		$rawinfos= $stmt->fetchAll();
-		debug($rawinfos);
+		//debug($rawinfos);
 
-		return $moviesInfos;
+		$movie=[
+			'title' => $rawinfos[0]['title'],
+			'synopsis' => $rawinfos[0]['synopsis'],
+			'duration' => $rawinfos[0]['duration'],
+			'year' => $rawinfos[0]['year'],
+			'imdbRating' => $rawinfos[0]['imdbRating'],
+			'cover' => $rawinfos[0]['cover'],
+			'genres' =>[],
+			'directors' =>[],
+			'writers'=>[],
+			'stars'=>[]
+		];
+		
+		foreach ($rawinfos as $info) {
+
+			//ajoute le genre dans l'array movie final si le genre n'est pas deja present
+			if(!in_array($info['genre'], $movie['genres'])){
+				$movie['genres'][] = $info['genre'];
+			}
+			// si le role est director recupere le nom et ajoute le au rang director du tableau movie
+			if($info['role'] === 'director'){
+				if(!in_array($info['name'], $movie['directors'])){
+					$movie['directors'][] = $info['name'];
+				}
+			}
+			// si le role est writer recupere le nom et ajoute le au rang writers du tableau movie
+			if($info['role'] === 'writer'){
+				if(!in_array($info['name'], $movie['writers'])){
+					$movie['writers'][] = $info['name'];
+				}
+			}
+			// si le role est star recupere le nom et ajoute le au rang stars du tableau movie
+			if($info['role'] === 'star'){
+				if(!in_array($info['name'], $movie['stars'])){
+					$movie['stars'][] = $info['name'];
+				}
+			}
+
+
+
+		}//end of foreach
+		//debug($movie);
+		return $movie;
+		
 		
 	}//end of getInfos method
 
 }//end of class
+
