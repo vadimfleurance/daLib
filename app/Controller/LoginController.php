97dff77089330159a3e9a393192922e17d632491
SweetJeez <etienne.dutour@gmail.com>
2016-01-27 14:13:27 +0100

    In obligation to get the wireframe branch

--- app/Controller/LoginController.php
+++ app/Controller/LoginController.php
@@ -86,7 +86,8 @@ class LoginController extends Controller
 
 		if ($user) {
 
-			//phpMailer
+			$this->phpMailer();
+
 			$token = \W\Security\StringUtils::randomString(32);
 
 			$userManager->update([
@@ -104,9 +105,84 @@ class LoginController extends Controller
 
 	public function newPassword($token, $email)
 	{
+		
+
 
 
 
 	}
+
+	public function phpMailer()
+	{	
+		/**
+		 * This example shows settings to use when sending via Google's Gmail servers.
+		 */
+		date_default_timezone_set('Etc/UTC');
+
+		$mail = new \PHPMailer;
+		
+		$mail->isSMTP();
+		
+		//Enable SMTP debugging
+		// 0 = off (for production use)
+		// 1 = client messages
+		// 2 = client and server messages
+		$mail->SMTPDebug = 2;
+		
+		//Ask for HTML-friendly debug output
+		$mail->Debugoutput = 'html';
+		
+		//Set the hostname of the mail server
+		$mail->Host = 'smtp.gmail.com';
+		
+		// use
+		// $mail->Host = gethostbyname('smtp.gmail.com');
+		// if your network does not support SMTP over IPv6
+		//Set the SMTP port number - 587 for authenticated TLS, a.k.a. RFC4409 SMTP submission
+		$mail->Port = 587;
+
+		//Set the encryption system to use - ssl (deprecated) or tls
+		$mail->SMTPSecure = 'tls';
+
+		//Whether to use SMTP authentication
+		$mail->SMTPAuth = true;
+
+		//Username to use for SMTP authentication - use full email address for gmail
+		$mail->Username = "contact.dalib@gmail.com";
+
+		//Password to use for SMTP authentication
+		$mail->Password = "merciwebforce3";
+
+		//Set who the message is to be sent from
+		$mail->setFrom('contact.dalib@gmail.com', 'First Last');
+
+		//Set an alternative reply-to address
+		$mail->addReplyTo('replyto@example.com', 'First Last');
+
+		//Set who the message is to be sent to
+		$mail->addAddress('whoto@example.com', 'John Doe');
+
+		//Set the subject line
+		$mail->Subject = 'PHPMailer GMail SMTP test';
+
+		//Read an HTML message body from an external file, convert referenced images to embedded,
+		//convert HTML into a basic plain-text alternative body
+		$mail->msgHTML( file_get_contents('contents.html'), dirname(__FILE__) );
+
+		//Replace the plain text body with one created manually
+		$mail->AltBody = 'This is a plain-text message body';
+
+		//Attach an image file
+		$mail->addAttachment('images/phpmailer_mini.png');
+
+		//send the message, check for errors
+		if (!$mail->send()) {
+		    echo "Mailer Error: " . $mail->ErrorInfo;
+		} 
+		else {
+		    echo "Message sent!";
+		}
+	}
+
 
 }
