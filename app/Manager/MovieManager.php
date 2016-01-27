16073202decb074e8150c86ee65cf72a845b4a46
vadim <supervad@gmail.com>
2016-01-27 11:03:16 +0100

    sql and statement done waiting for valid database to continue

--- app/Manager/MovieManager.php
+++ app/Manager/MovieManager.php
@@ -20,7 +20,35 @@ class MovieManager extends \W\Manager\Manager
 
 	public function getInfos($id)
 	{
-		$sql =
-	}
+		
 
-}
+		$sql = 'SELECT 
+				movies.title,
+				movies.synopsis,
+				movies.duration,
+				movies.year,
+				movies.imdbRating,
+				movies.cover,
+				genres.name,
+				movies__humans.role,
+				humans.name
+				FROM movies
+				LEFT JOIN movies__genres
+				ON movies.id = movies__genres.idMovie
+				LEFT JOIN genres
+				ON genres.id = movies__genres.idGenre
+				LEFT JOIN movies__humans
+				ON movies.id = movies__humans.idMovie
+				LEFT JOIN humans
+				ON movies__humans.idHuman = humans.id
+				WHERE movies.id =:id';
+
+		$stmt = $this->dbh->prepare($sql);
+		$stmt->bindValue(':id', $id);
+		$stmt->execute();
+		$rawinfos= $stmt->fetchAll();
+		debug($rawinfos);
+		
+	}//end of getInfos method
+
+}//end of class
