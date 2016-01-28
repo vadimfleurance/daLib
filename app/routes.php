986c005fdd1116770cd4154a1ec6ecc387101183
Flop2691 <flo.bardouillet@gmail.com>
2016-01-28 15:42:05 +0100

    Suppression route test

--- app/routes.php
+++ app/routes.php
@@ -3,7 +3,6 @@
 	$w_routes = array(
 		['GET', '/', 'Default#home', 'home'],
 		['GET', '/scrap/', 'Scraper#globalScraper', 'global_scraper'],
-		['GET', '/scraptest/', 'Scraper#MovieInsert', 'MovieInsert'],
 		['GET', '/movie-detail/[i:id]/', 'Movie#movieDetails', 'movie_detail'],
 		['GET|POST', '/add-movie/', 'Movie#addMovie', 'add_movie'],
 	);
