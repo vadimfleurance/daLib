3b4dada20d5cdd25b804c87835735d22976beec4
vadim <supervad@gmail.com>
2016-01-28 15:47:47 +0100

    clean routes

--- app/routes.php
+++ app/routes.php
@@ -3,7 +3,6 @@
 	$w_routes = array(
 		['GET', '/', 'Default#home', 'home'],
 		['GET', '/scrap/', 'Scraper#globalScraper', 'global_scraper'],
-		['GET', '/scraptest/', 'Scraper#MovieInsert', 'MovieInsert'],
 		['GET', '/movie-detail/[i:id]/', 'Movie#movieDetails', 'movie_detail'],
 		['POST', '/addtocollection/', 'Collection#addToCollection', 'add_to_collection'],
 	);
