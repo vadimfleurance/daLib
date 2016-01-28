37d3002939820b265a327425ec75a70244324b19
D-Grimm <gregory.careme@gmail.com>
2016-01-28 15:21:40 +0100

    Wireframe | Movie Details
    Réalisation de la page - Implantation des variables dans le html
    + Mise en forme
    + Circle rating

--- app/templates/movie/details.php
+++ app/templates/movie/details.php
@@ -1,46 +1,125 @@
-<?php $this->layout('layout', ['title' => 'Movie Details', 'nav_title' => 'daLib']) ?>
+<?php $this->layout('layout', ['title' => 'Movie Details | Your collection of movies in your pocket']) ?>
 
 <?php $this->start('main_content') ?>
-	<article>
-		<h1><?= $movie['title'] ?></h1>
-		<b>Synopsis:</b>
-		<p><?= $movie['synopsis'] ?></p>
-		<b>Duration:</b>
-		<p><?= $movie['duration'] ?></p>
-		<b>Year:</b>
-		<p><?= $movie['year'] ?></p>
-		<b>Genres:</b>
-		<p class="genres">
-			<?php foreach ($movie['genres'] as $genre): ?>
-				<span><?= $genre ?></span>
-				
-			<?php endforeach ?>
-		</p>
-		<b>Directors:</b>
-		<p class="directors">
-			<?php foreach ($movie['directors'] as $director): ?>
-				<span><?= $director ?></span>
-				
-			<?php endforeach ?>
-		</p>
-		<b>Writers:</b>
-		<p class="writers">
-			<?php foreach ($movie['writers'] as $writer): ?>
-				<span><?= $writer ?></span>
-				
-			<?php endforeach ?>
-		</p>
-		<b>Stars:</b>
-		<p class="stars">
-			<?php foreach ($movie['stars'] as $star): ?>
-				<span><?= $star ?></span>
-				
-			<?php endforeach ?>
-		</p>
-		<b>Rating:</b>
-		<p><?= $movie['imdbRating'] ?></p>
-		<img src="<?= $movie['cover'].'@._V1_SY450_.jpg' ?>"/>
-	</article>
-	
+	<section id="movie-details">
+		<!-- <article class="container">
+			<div class="row">
+				<div class="col-xs-12 col-sm-4">
+					<figure>
+						<img src="<?= $movie['cover'].'@._V1_SY300_.jpg' ?>" class="img-responsive" alt="Cover <?= $movie['title'] ?>">
+					</figure>
+				</div>
+				<div class="col-xs-12 col-sm-8">
+					<header>
+						<h1><?= $movie['title'] ?> <small>(<?= $movie['year'] ?>)</small></h1>
+						<p class="genre">
+							<?php foreach ($movie['genres'] as $genre):?>
+								<span><?= $genre ?></span>
+							<?php endforeach ?>
+						</p>
+					</header>
+					<dl class="row">
+						<dt class="col-xs-4 col-sm-3">Duration</dt>
+						<dd class="col-xs-8 col-sm-9"><?= $movie['duration'] ?> min</dd>
+						<dt class="col-xs-4 col-sm-3">Directors</dt>
+						<dd class="col-xs-8 col-sm-9">
+							<?php foreach ($movie['directors'] as $director): ?>
+								<span><?= $director ?></span>
+							<?php endforeach ?>
+						</dd>
+						<dt class="col-xs-4 col-sm-3">Writers</dt>
+						<dd class="col-xs-8 col-sm-9">
+							<?php foreach ($movie['writers'] as $writer): ?>
+								<span><?= $writer ?></span>
+							<?php endforeach ?>
+						</dd>
+						<dt class="col-xs-4 col-sm-3">Stars</dt>
+						<dd class="col-xs-8 col-sm-9">
+							<?php foreach ($movie['stars'] as $star): ?>
+								<span><?= $star ?></span>
+							<?php endforeach ?>
+						</dd>
+					</dl>
+				</div>
+			</div>
+
+			<div class="row">
+				<div class="col-xs-12 col-sm-4">
+					<div class="c100 p<?= $movie['imdbRating']*10 ?> big">
+						<span><?= $movie['imdbRating'] ?></span>
+						<div class="slice">
+							<div class="bar"></div>
+							<div class="fill"></div>
+						</div>
+					</div>
+				</div>
+				<div class="col-xs-12 col-sm-8">
+					<p><?= $movie['synopsis'] ?></p>
+				</div>
+			</div>
+		</article> -->
+
+		<article class="container">
+			<div class="row">
+				<div class="col-xs-12 col-sm-4">
+					<div class="row">
+						<div class="col-xs-12">
+							<figure>
+								<img src="<?= $movie['cover'].'@._V1_SY500_.jpg' ?>" class="img-responsive center-block" alt="Cover <?= $movie['title'] ?>">
+							</figure>
+						</div>
+						
+					</div>		
+				</div>
+
+				<div class="col-xs-12 col-sm-8">
+					<header>
+						<h1><?= $movie['title'] ?> <small>(<?= $movie['year'] ?>)</small></h1>
+						<p class="genre">
+							<?php foreach($movie['genres'] as $genre):?>
+								<span><?= $genre ?></span>
+							<?php endforeach ?>
+						</p>
+					</header>
+					<div class="row">
+						<div class="col-xs-12 col-sm-8">
+							<dl class="row">
+								<dt class="col-xs-4 col-sm-3">Duration</dt>
+								<dd class="col-xs-8 col-sm-9"><?= $movie['duration'] ?> min</dd>
+								<dt class="col-xs-4 col-sm-3">Directors</dt>
+								<dd class="col-xs-8 col-sm-9">
+									<?php foreach($movie['directors'] as $director): ?>
+										<span><?= $director ?></span>
+									<?php endforeach ?>
+								</dd>
+								<dt class="col-xs-4 col-sm-3">Writers</dt>
+								<dd class="col-xs-8 col-sm-9">
+									<?php foreach($movie['writers'] as $writer): ?>
+										<span><?= $writer ?></span>
+									<?php endforeach ?>
+								</dd>
+								<dt class="col-xs-4 col-sm-3">Stars</dt>
+								<dd class="col-xs-8 col-sm-9">
+									<?php foreach($movie['stars'] as $star): ?>
+										<span><?= $star ?></span>
+									<?php endforeach ?>
+								</dd>
+							</dl>
+						</div>
+						<div class="col-xs-12 col-sm-4">
+							<div class="c100 p<?= $movie['imdbRating']*10 ?> small">
+								<span><?= $movie['imdbRating'] ?></span>
+								<div class="slice">
+									<div class="bar"></div>
+									<div class="fill"></div>
+								</div>
+							</div>
+						</div>
+					</div>
+					<p><?= $movie['synopsis'] ?></p>
+				</div>
+			</div>
+		</article>
+	</section>
 
 <?php $this->stop('main_content') ?>
