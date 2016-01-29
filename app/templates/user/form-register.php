2f53d1e0059a23060682de6ff5c44f41a38a18c2
D-Grimm <gregory.careme@gmail.com>
2016-01-29 10:00:34 +0100

    Wireframe | Custom Login & Register
    Peaufinage des formulaires login & register

--- app/templates/user/form-register.php
+++ app/templates/user/form-register.php
@@ -1,3 +1,27 @@
+<!-- 
+	Les erreurs sont envoyées dans un tableau associatif via le fichier "register.php" 
+	qui insert le fichier courant ("form-register.php"). 
+	C'est lors de cet insert qu'est passé en argument le tableau "$errors". 
+	Ce tableau contient les erreurs correspondantes à chaque champs de saisi 
+	du formulaire sous forme d'index : username, email, password et total.
+	Cette étape est effectuée dans "UserController.php" dans sa méthode "register".
+
+	Le "foreach" permet de pouvoir les afficher aux différents endroits et en fonctions du champs
+	mal renseigné. 
+
+	Le message d'erreur "$errors['total']" est appelé en bas du code car c'est le cas où aucun champs
+	est correctement renseigné.
+-->
+<!-- Message d'erreur pour une mauvaise saisie du formulaire -->
+<?php if(!empty($errors['total'])): ?>
+	<div class="alert alert-danger alert-dismissible" role="alert">
+		<button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+		<?php foreach($errors['total'] as $error ): ?>
+			<p><strong>Error !</strong> <?= $error; ?></p>
+		<?php endforeach; ?>
+	</div>
+<?php endif; ?>
+
 <form class="clearfix" method="POST" action="<?= $this->url('register')?>">
 	<div class="col-xs-12">
 		<div class="form-group">
@@ -6,32 +30,14 @@
 				<div class="input-group-addon"><i class="fa fa-user"></i></div>
 				<input type="text" class="form-control" id="userRegisterInput" name="user[username]" placeholder="Username">
 			</div>
-
-			<!-- 
-				Les erreurs sont envoyées dans un tableau associatif via le fichier "register.php" 
-				qui insert le fichier courant ("form-register.php"). 
-				C'est lors de cet insert qu'est passé en argument le tableau "$errors". 
-				Ce tableau contient les erreurs correspondantes à chaque champs de saisi 
-				du formulaire sous forme d'index : username, email, password et total.
-				Cette étape est effectuée dans "UserController.php" dans sa méthode "register".
-
-				Le "foreach" permet de pouvoir les afficher aux différents endroits et en fonctions du champs
-				mal renseigné. 
-
-				Le message d'erreur "$errors['total']" est appelé en bas du code car c'est le cas où aucun champs
-				est correctement renseigné.
-			-->
-			
 			<!-- Message d'erreur pour un USERNAME déjà utilisé  -->
-			<?php 
-				if (!empty( $errors['username'] )) : 
-					foreach ( $errors['username'] as $error ) : ?>
-						<p><?= $error; ?></p>
-					<?php endforeach ;
-				endif ; 
-			?>
-			<!-- -->
-
+			<?php if (!empty( $errors['username'] )) : ?>
+				<ul class="text-danger">
+				<?php foreach ( $errors['username'] as $error ) : ?>
+					<li><?= $error; ?></li>
+				<?php endforeach; ?>
+				</ul>
+			<?php endif; ?>
 		</div>
 	</div>
 	<div class="col-xs-12">
@@ -41,17 +47,14 @@
 				<div class="input-group-addon"><i class="fa fa-envelope"></i></div>
 				<input type="email" class="form-control" id="emailRegisterInput" name="user[email]" placeholder="Email">
 			</div>
-
-			<!-- Message d'erreur pour une adresse EMAIL déjà utilisé -->
-			<?php 
-				if (!empty( $errors['email'] )) : 
-					foreach ( $errors['email'] as $error ) : ?>
-						<p><?= $error; ?></p>
-					<?php endforeach ;
-				endif ; 
-			?>
-			<!-- -->
-
+			<!-- Message d'erreur pour un USERNAME déjà utilisé  -->
+			<?php if (!empty( $errors['email'] )) : ?>
+				<ul class="text-danger">
+				<?php foreach ( $errors['email'] as $error ) : ?>
+					<li><?= $error; ?></li>
+				<?php endforeach; ?>
+				</ul>
+			<?php endif; ?>
 		</div>
 	</div>
 	<div class="col-xs-12">
@@ -70,31 +73,17 @@
 				<div class="input-group-addon"><i class="fa fa-lock"></i></div>
 				<input type="password" class="form-control" id="passwordConfirmRegisterInput" name="user[passwordBis]" placeholder="Password Confirm">
 			</div>
-
-			<!-- Message d'erreur pour une mauvaise saisie du PASSWORD -->
-			<?php 
-				if (!empty( $errors['password'] )) : 
-					foreach ( $errors['password'] as $error ) : ?>
-						<p><?= $error; ?></p>
-					<?php endforeach ;
-				endif ; 
-			?>
-			<!-- -->
-
+			<!-- Message d'erreur pour un USERNAME déjà utilisé  -->
+			<?php if (!empty( $errors['password'] )) : ?>
+				<ul class="text-danger">
+				<?php foreach ( $errors['password'] as $error ) : ?>
+					<li><?= $error; ?></li>
+				<?php endforeach; ?>
+				</ul>
+			<?php endif; ?>
 		</div>
 	</div>
-
-	<!-- Message d'erreur pour une mauvaise saisie du formulaire -->
-	<?php 
-		if (!empty( $errors['total'] )) : 
-			foreach ( $errors['total'] as $error ) : ?>
-				<p><?= $error; ?></p>
-			<?php endforeach ;
-		endif ; 
-	?>
-	<!-- -->
-
 	<div class="col-xs-12">
-		<button type="submit" class="btn btn-default btn-block" name="action[register]" value="register" >Register</button>
+		<button type="submit" class="btn btn-default btn-block btn-register" name="action[register]" value="register" >Register</button>
 	</div>
 </form>
