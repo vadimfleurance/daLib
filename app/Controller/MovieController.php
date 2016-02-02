e1448ba9c032f70dd78dc2197c8d12162be1f4ee
Flop <flo.bardouillet@gmail.com>
2016-02-02 20:28:30 +0100

    Ajout de films
    Gère maintenant les erreurs 404 des pages d'IMDb

--- app/Controller/MovieController.php
+++ app/Controller/MovieController.php
@@ -60,41 +60,49 @@ class MovieController extends Controller
 
 	public function addMovie()
 	{
-		//variable qui s'affiche dans le template et qui dit si le film a été ajouté, s'il est déjà présent en base de données ou si l'url n'est pas correcte
+		//s'affiche dans le template si le film a été ajouté
 		$errorScrap = "";
+		//s'affiche dans le template si le film est déjà présent en base de données ou si l'url n'est pas correcte
 		$successScrap = "";
 
 		//si le formulaire est soumis
 		if ($_POST){
-			$url = $_POST["add-movie-input"];
 
 			//vérification que l'url est correcte
 			$scraperController = new \Controller\ScraperController;
-			$results = $scraperController->verifyUrl($url);
+			$url = $scraperController->verifyUrl($_POST['add-movie-input']);
 
 			//si l'url est correcte
-			if ($results){
+			if ($url){
+				$headerStatus = $scraperController->verifyHeader($url);
 
-				//récupère la référence IMDb du lien que l'utilisateur a donné puis vérifie s'il est présent dans la base de données
-				preg_match("!tt\d{7}!", $results, $matches);
-				$movie["imdbRef"] = $matches[0];
-				$movieManager = new \Manager\MovieManager;
-				$isNew = $movieManager->isNew($movie);
-				//si le film n'est pas présent dans la base de données
-				if($isNew){
-					$scraperController->pageScraper($results);
-					$successScrap = "The movie has been added.";
+				//si l'url ne renvoie pas vers une erreur 404 d'IMDb
+				if($headerStatus){
+					//récupère la référence IMDb du lien que l'utilisateur a donné puis vérifie s'il est présent dans la base de données
+					preg_match("!tt\d{7}!", $url, $matches);
+					$movie["imdbRef"] = $matches[0];
+					$movieManager = new \Manager\MovieManager;
+					$isNew = $movieManager->isNew($movie);
+					//si le film n'est pas présent dans la base de données
+					if($isNew){
+						$scraperController->pageScraper($url);
+						$successScrap = "The movie has been added.";
+					}
+
+					//si le film est déjà présent dans la base de données
+					else{
+						$errorScrap = "The movie is already present in our database.";
+					}					
 				}
-
-				//si le film est déjà présent dans la base de données
+				//si l'url renvoie vers une erreur 404
 				else{
-					$errorScrap = "The movie is already present in our database.";
+					$errorScrap = "IMDb reference is invalid";
 				}
 			}
 
 			//si l'url n'est pas correcte
 			else{
-				$errorScrap = "URL is not valid.";
+				$errorScrap = "URL is invalid.";
 			}
 
 		}
