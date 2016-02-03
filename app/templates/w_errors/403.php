0b8fd18fe881391a2def4f43d5e9cb47de752daf
D-Grimm <gregory.careme@gmail.com>
2016-02-04 00:15:25 +0100

    Fix | Page Errors
    Design des pages 404 & 403
    + Add title aux liens

--- app/templates/w_errors/403.php
+++ app/templates/w_errors/403.php
@@ -1,5 +1,14 @@
 <?php $this->layout('layout', ['title' => 'Créer un nouvel administrateur']) ?>
 
 <?php $this->start('main_content'); ?>
-<h1>403. Nothing to see here.</h1>
+<section class="section-padding">
+	<div class="container">
+		<div class="row">
+			<div class="col-xs-12 col-md-8 col-md-offset-2">
+				<h1>403. Nothing to see here.</h1>
+				<a href="<?= $this->url('home') ?>" title="Back to the homepage">Back to home</a>
+			</div>
+		</div>
+	</div>
+</section>
 <?php $this->stop('main_content'); ?>
--- app/templates/w_errors/404.php
+++ app/templates/w_errors/404.php
@@ -1,7 +1,14 @@
 <?php $this->layout('layout', ['title' => 'Perdu ?']) ?>
 
 <?php $this->start('main_content'); ?>
-<h1>404: Would you be trying to cut your steak with a fork? <i class="fa fa-hand-scissors-o"></i></h1>
-<a href="<?= $this->url('home') ?>">back to home you naughty little chimp!!!</a>
+<section class="section-padding">
+	<div class="container">
+		<div class="row">
+			<div class="col-xs-12 col-md-8 col-md-offset-2">
+				<h1>404: Would you be trying to cut your steak with a fork? <i class="fa fa-hand-scissors-o"></i></h1>
+				<a href="<?= $this->url('home') ?>" title="Back to the homepage">back to home you naughty little chimp!!!</a>
+			</div>
+		</div>
+	</div>
+</section>
 <?php $this->stop('main_content'); ?>
-	
